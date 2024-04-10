dbad22aa97d5d524d16f37ddd1ada4175c494434
Sala G3 <CSalaG3@uniboyaca.edu.co>
2024-04-09 19:54:12 -0500

    Holaaa

---
+++ ejemplo-vue/src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+// aqui va el js 
+
+// - - - trabajado como opciones; funciones- - -
+// export default{
+//   data(){
+//     return {
+//       saludo: 'Hola Mundo'
+//     }
+//   }
+// }
+
+// // - - - trabajando por objetos o components - - - 
+// import {ref} from 'vue';
+//   const saludo = ref('Hola Mundo!!!');
+//   // usa programacion reactiva 
+</script>
+
+<template>
+  <!-- aqui va el html  -->
+
+  <!-- se agrega contenido mediante variables al html  -->
+  <h1>{{ saludo }}</h1>
+</template>
+
+<!-- scoped para solo el componente pero sin el queda global los estilos  -->
+<style scoped>
+  /* aqui va el css  */
+
+</style>

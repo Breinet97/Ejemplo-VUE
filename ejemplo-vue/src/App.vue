d53eed15d6142403d643071793293276f7e1d3c3
JHONATHANUNI <jfuni@uniboyaca.edu.co>
2024-04-19 11:08:15 -0500

    Primeros Pasos

--- ejemplo-vue/src/App.vue
+++ ejemplo-vue/src/App.vue
@@ -1,30 +1,52 @@
-<script setup>
 // aqui va el js 
+<script setup>
+// el setup indica el tipo, si es por opciones o composicion 
+// ademas usa programacion reactiva 
 
-// - - - trabajado como opciones; funciones- - -
-// export default{
-//   data(){
-//     return {
-//       saludo: 'Hola Mundo'
-//     }
-//   }
-// }
+const name = 'USUARIO';
 
-// // - - - trabajando por objetos o components - - - 
-// import {ref} from 'vue';
-//   const saludo = ref('Hola Mundo!!!');
-//   // usa programacion reactiva 
+const color= "color: blue";
+const colorParrafo= "color: orange";
+
+const vueText= 'VUE';
+
+const arrayColor= ["yellow","blue","red"];
 </script>
 
+<!-- aqui va el html  -->
 <template>
-  <!-- aqui va el html  -->
+  <!-- en html vanila, osea el html puro. No se podria hacer esto y tendriamos que hacer más pasos con el js  --> 
+  <h1>Hola {{ name }}</h1>
+  <h2>Agregar Variables a HTML con JS en VUE</h2>
 
-  <!-- se agrega contenido mediante variables al html  -->
-  <h1>{{ saludo }}</h1>
+  <p>El titulo fue impreso mediante la interpolacion de texto.</p>
+
+  <p>Esta interpolacion no se puede usar en atributos, solo variables.</p>
+  
+  <p>Esto es VUE Dinamico.</p>
+
+  <h2 v-bind:style="color">v-bind para agregar estilos y variables js</h2>
+  <p>El color azul de este titulo fue agregado mediante v-bind, que es una manera de agregar una variable a un atributo.<br>
+  Es como tener la interpolaricacion pero para atributos. 
+  </p>
+  <p :style="colorParrafo">Una manera de escribirlo a breviado es con: <br>
+      :style="color" <br>   
+      Que es lo mismo que escribir: <br>
+      v-bind:style="color"
+  </p>
+  <h2>Agregar funciones de JS en html</h2>
+  <h3>Me gusta mucho {{ vueText.toLowerCase() }}</h3>
+  <p>Se agrega solo expresiones no todo un js</p>
 </template>
 
-<!-- scoped para solo el componente pero sin el queda global los estilos  -->
-<style scoped>
-  /* aqui va el css  */
-
+<!-- estilos o css  -->
+<style>
+h1{
+  color: red;
+  font-size: 5rem;
+}
+h2{
+  color: rgb(100, 100, 206);
+  font-size: 3rem;
+}
 </style>
